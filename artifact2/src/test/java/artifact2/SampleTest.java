d4d4c1e1272487899365060e8973185d5c1b4bd7
DWARF <DWARF@DWARF-PC>
2017-05-04 00:17:29 +0300

    initial

---
+++ artifact2/src/test/java/artifact2/SampleTest.java
@@ -0,0 +1,21 @@
+package artifact2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void test1(){
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+	@Test
+	public void test2(){
+		int a = 2;
+		int b = 2;
+		assertTrue(a + b == 4);
+	}
+
+}
